4cef236c0e5418b2b44cb2c2d06b1295e02d819d
Victor Ferro <47899434+vsosmonteiro@users.noreply.github.com>
2023-11-12 01:57:22 -0300

    adding buttons to onboarding screen and removing status bar color /video4

--- app/src/main/java/com/loc/newsapp/presentation/onboarding/Dimens.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/Dimens.kt
@@ -5,5 +5,8 @@ import androidx.compose.ui.unit.dp
 object Dimens {
     val MediumPadding1 = 24.dp
     val MediumPadding2 = 30.dp
+
     val IndicatorSize = 14.dp
+
+    val PageIndicatorWidth=52.dp
 }
--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnboardingScreen.kt
@@ -1,15 +1,30 @@
 package com.loc.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.loc.newsapp.presentation.common.NewsButton
+import com.loc.newsapp.presentation.common.NewsTextButton
+import com.loc.newsapp.presentation.onboarding.Dimens.MediumPadding2
+import com.loc.newsapp.presentation.onboarding.Dimens.PageIndicatorWidth
 import com.loc.newsapp.presentation.onboarding.components.OnBoardingPage
+import com.loc.newsapp.presentation.onboarding.components.PageIndicator
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -32,5 +47,47 @@ fun OnboardingScreen() {
             OnBoardingPage(page = pages[index])
 
         }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                pageSize = pages.size,
+                selectedPage = pageState.currentPage,
+                modifier = Modifier.width(PageIndicatorWidth)
+            )
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val scope = rememberCoroutineScope()
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(
+                        text = buttonState.value[0],
+                        onClick = {
+                            scope.launch {
+                                pageState.animateScrollToPage(page = pageState.currentPage - 1)
+
+                            }
+                        },
+                    )
+                }
+                NewsButton(
+                    text = buttonState.value[1],
+                    onClick = {
+                        scope.launch {
+                            if (pageState.currentPage == 3) {
+                                //todo navigate to home
+                            }
+                            pageState.animateScrollToPage(page = pageState.currentPage + 1)
+
+                        }
+                    },
+                )
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.5f))
     }
 }
